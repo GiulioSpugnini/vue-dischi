2df5020a91b3e885e1896f2ecef6eedb3cdbc16c
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-10 18:13:49 +0100

    last commit

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="container">
-    <ul class="d-flex flex-wrap justify-content-between my-2">
-      <li id="album" class="text-center m-3 p-2" v-for="album in albums" :key="album.title">
+    <ul class="d-flex flex-wrap justify-content-between my-1">
+      <li id="album" class="text-center m-2 p-2" v-for="album in albums" :key="album.title">
         <img class="p-2" :src="album.poster" alt="">
-        <h3 class="p-2">{{ album.title.toUpperCase() }}</h3>
+        <h4 class="p-2">{{ album.title.toUpperCase() }}</h4>
         <h5 class="text-secondary">{{ album.author }}</h5>
         <h6 class="text-secondary">{{ album.year }}</h6>
       </li>
