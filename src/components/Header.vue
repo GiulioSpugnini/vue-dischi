0748c21a0ad2cb4eeaf76023beff653654754d5a
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-10 16:17:49 +0100

    implemented Header.vue graphics and bg-colors of the page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,20 @@
 <template>
-  <div></div>
+  <header>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+      integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <i class="fab fa-spotify"></i>
+  </header>
 </template>
 
 <script>
 export default {
-name: 'Header',
-}
+  name: "Header",
+};
 </script>
 
 <style>
